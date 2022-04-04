d9f1462bd9d0ae4dc949960616d2250fc5b92a36
Diego <diegobarbozacoto@gmail.com>
2022-04-03 22:23:35 -0600

    agregando metodos insert en BLCliente / BLEspecialidadesMedicas
    agregando metodos insert en BLCliente / BLEspecialidadesMedicas

---
+++ Proyecto/Proyecto/BL/BLCliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto.Modelos;
+namespace Proyecto.BL
+{
+    public class BLCliente
+    {
+        CentroMedicoEntities modeloBD = new CentroMedicoEntities();
+
+        public bool InsertarClientes(int cedula, string genero, string fechaNacimiento, string nombre, string primerApellido, string segundoApellido,
+                                      string telefono1,string telefono2, string correo, int idProvincia, int idCanton, int idDistrito, string direccion)
+        {
+            int registrosAfectados = 0;
+
+            registrosAfectados = modeloBD.sp_InsertarCliente(cedula, genero,fechaNacimiento,nombre,primerApellido,segundoApellido, telefono1, 
+                                                             telefono2, correo, idProvincia, idCanton, idDistrito, direccion);
+
+            if (registrosAfectados>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Proyecto/Proyecto/BL/BLEspecialidadesMedicas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto.Modelos;
+namespace Proyecto.BL
+{
+    public class BLEspecialidadesMedicas
+    {
+        CentroMedicoEntities modeloBD = new CentroMedicoEntities();
+
+        public bool insertarEspecialidadMedica(string nombre, string descripcion)
+        {
+            int registrosAfectados = 0;
+
+            registrosAfectados = modeloBD.sp_InsertarEspecialidadMedica(nombre, descripcion);
+
+            if (registrosAfectados > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
