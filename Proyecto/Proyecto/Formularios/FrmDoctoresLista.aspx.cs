779a6d0bfba7d6ca424ffb93b24cf5f3dd76c5a8
Monserrat Picado <monse201014@gmail.com>
2022-04-14 11:39:11 -0600

    Agrego frm doctores lista y consultas
    Agrego frm doctores lista y consultas

---
+++ Proyecto/Proyecto/Formularios/FrmDoctoresLista.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Proyecto.BL;
+using Proyecto.Modelos;
+
+namespace Proyecto.Formularios
+{
+    public partial class FrmDoctoresLista : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            cargarDatos();
+        }
+
+        void cargarDatos()
+        {
+            BlDoctores oBlDoctores = new BlDoctores();
+
+            try
+            {
+                List<sp_ConsultarDoctoresPorCedula_Result> listaDoctores = new List<sp_ConsultarDoctoresPorCedula_Result>();
+                listaDoctores = oBlDoctores.RetornarDoctores(txtCedula.Text);
+                grdDoctores.DataSource = listaDoctores;
+                grdDoctores.DataBind();
+            }
+            catch (Exception excepcion)
+            {
+
+                lblMensajeError.Text = "ocurrio un error al retornar los datos" + excepcion;
+
+            }
+        }
+
+
+
+
+    }
+}
